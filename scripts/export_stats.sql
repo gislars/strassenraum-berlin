d8c5b6795645442c626e90ab05d4f16e9b62fd73
gislars <lars@lingner.eu>
2022-07-21 22:45:04 +0200

    Update stats, include references to parent level

--- scripts/export_stats.sql
+++ scripts/export_stats.sql
@@ -1,6 +1,47 @@
-SELECT json_build_object(
+WITH stats AS (
+SELECT
+  id,
+  name,
+  (
+    SELECT
+      id
+    FROM
+      boundaries_stats b1
+    WHERE
+      ST_Contains(b1.geom, ST_PointOnSurface(b.geom))
+      AND b1.admin_level < b.admin_level
+      AND b1.admin_level = 9
+  ) parent_id ,
+  (
+    SELECT
+      name
+    FROM
+      boundaries_stats b1
+    WHERE
+      ST_Contains(b1.geom, ST_PointOnSurface(b.geom))
+      AND b1.admin_level < b.admin_level
+      AND b1.admin_level = 9
+  ) parent_name,
+  admin_level,
+  aera_sqkm,
+  street_side_km,
+  lane_km,
+  d_other_km,
+  sum_km,
+  length_wo_dual_carriageway,
+  done_percent,
+  geom
+FROM
+  boundaries_stats b
+WHERE
+  b.name NOT IN ('Brandenburg')
+ORDER BY b.admin_level, b.name
+)
+SELECT
+  json_build_object(
     'type', 'FeatureCollection',
     'features', json_agg(ST_AsGeoJSON(t.*)::json)
-    )
-FROM boundaries_stats t
-     ;
+  )
+FROM
+  stats t
+;
